09c137c64036c101a2be7d0164d21f71b9d90be1
Elias Schmid <elischmi@student.ethz.ch>
2020-12-09 11:38:57 +0100

    Add solutions for day 9

--- 2020/day09/task2.py
+++ 2020/day09/task2.py
@@ -7,6 +7,7 @@ def main():
     data = [int(x) for x in file.readlines()]
     file.close()
 
+    specialNum = 0
     summands = sorted(data[:25])
     for i, num in enumerate(data[25:], 25):
         sumExists = False
@@ -23,12 +24,25 @@ def main():
                 break
 
         if not sumExists:
-            print(num)
+            specialNum = num
             break
 
         summands.remove(data[i-25])
         summands.append(num)
         summands.sort()
+
+    left = 0
+    right = 1
+    localSum = data[0] + data[1]
+    while localSum != specialNum:
+        if localSum < specialNum:
+            right += 1
+            localSum += data[right]
+        else:
+            localSum -= data[left]
+            left += 1
+
+    print(min(data[left:right+1]) + max(data[left:right+1]))
 
 
 if __name__ == "__main__":
