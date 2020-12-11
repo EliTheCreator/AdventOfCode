c0a7d9c590fa7a57c955412d36a7b5f5e4a920d3
Elias Schmid <elischmi@student.ethz.ch>
2020-12-11 11:48:11 +0100

    Add solutions for day 11

--- 2020/day11/task2.py
+++ 2020/day11/task2.py
@@ -1,5 +1,21 @@
 from copy import deepcopy
 from hashlib import md5
+from time import sleep
+
+
+def check(lookup, i, j, iOffset, jOffset):
+    _i = iOffset
+    _j = jOffset
+
+    while 0 < i + _i < len(lookup) and 0 < j + _j < len(lookup[0]):
+        if lookup[i + _i][j + _j] == '#':
+            return 1
+        elif lookup[i + _i][j + _j] == 'L':
+            return 0
+        _i += iOffset
+        _j += jOffset
+
+    return 0
 
 
 def main():
@@ -19,10 +35,14 @@ def main():
         for i in range(1, len(seating) - 1):
             for j in range(1, len(seating[0]) - 1):
                 taken = 0
-                for i_ in range(-1, 2):
-                    for j_ in range(-1, 2):
-                        if lookup[i + i_][j + j_] == '#':
-                            taken += 1
+                taken += check(lookup, i, j, -1, 0)
+                taken += check(lookup, i, j, -1, 1)
+                taken += check(lookup, i, j, 0, 1)
+                taken += check(lookup, i, j, 1, 1)
+                taken += check(lookup, i, j, 1, 0)
+                taken += check(lookup, i, j, 1, -1)
+                taken += check(lookup, i, j, 0, -1)
+                taken += check(lookup, i, j, -1, -1)
 
                 seat = lookup[i][j]
                 if seat == 'L' and taken == 0:
@@ -30,7 +50,7 @@ def main():
                 elif seat == '#' and taken > 4:
                     seating[i][j] = 'L'
 
-        hashed = md5(''.join([''.join(x)
+        hashed = md5(''.join(['\n'.join(x)
                               for x in seating]).encode()).digest()
         if hashed in iterations:
             taken = sum([len(list(filter(lambda c: c == '#', row)))
