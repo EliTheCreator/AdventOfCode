661efdf28b74bab29ee10bddc81b5017d3f3a545
Elias Schmid <elischmi@student.ethz.ch>
2020-12-17 13:23:10 +0100

    Add solutions for day 17

--- 2020/day17/task2.py
+++ 2020/day17/task2.py
@@ -5,36 +5,38 @@ def main():
     for x, line in enumerate(file.readlines()):
         for y, field in enumerate(line.strip()):
             if field == '#':
-                active.add((x, y, 0))
+                active.add((x, y, 0, 0))
     file.close()
 
     for _ in range(6):
         to_check = set()
         next_active = set()
 
-        for x, y, z in active:
+        for x, y, z, w in active:
             active_count = 0
             for xo in range(-1, 2):
                 for yo in range(-1, 2):
                     for zo in range(-1, 2):
-                        if (x+xo, y+yo, z+zo) in active:
-                            active_count += 1
-                        else:
-                            to_check.add((x+xo, y+yo, z+zo))
+                        for wo in range(-1, 2):
+                            if (x+xo, y+yo, z+zo, w+wo) in active:
+                                active_count += 1
+                            else:
+                                to_check.add((x+xo, y+yo, z+zo, w+wo))
 
             if 2 < active_count < 5:
-                next_active.add((x, y, z))
+                next_active.add((x, y, z, w))
 
-        for x, y, z in to_check:
+        for x, y, z, w in to_check:
             active_count = 0
             for xo in range(-1, 2):
                 for yo in range(-1, 2):
                     for zo in range(-1, 2):
-                        if (x+xo, y+yo, z+zo) in active:
-                            active_count += 1
+                        for wo in range(-1, 2):
+                            if (x+xo, y+yo, z+zo, w+wo) in active:
+                                active_count += 1
 
             if active_count == 3:
-                next_active.add((x, y, z))
+                next_active.add((x, y, z, w))
 
         active = next_active
 
