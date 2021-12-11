d6c677239c39844a1a7f18f292ff4922dcf2f569
Elias Schmid <elischmi@ethz.ch>
2021-12-11 21:53:09 +0000

    Add solutions for 2015 day 10

---
+++ 2015/day10/task1.py
@@ -0,0 +1,27 @@
+
+def main():
+    with open("input", "r") as file:
+        puzzleInput = file.readline().strip()
+
+    prev = puzzleInput
+    next = ""
+    for _ in range(40):
+        cur = prev[0]
+        counter = 1
+        for c in prev[1:]:
+            if c == cur:
+                counter += 1
+            else:
+                next += str(counter) + cur
+                cur = c
+                counter = 1
+
+        next += str(counter) + cur
+        prev = next
+        next = ""
+
+    print(len(prev))
+
+
+if __name__ == "__main__":
+    main()
