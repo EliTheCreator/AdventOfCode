aa78de7dcd746db38818ca1ed3b5eb22ffadcb64
Elias Schmid <elischmi@ethz.ch>
2021-12-13 10:51:42 +0000

    Add solutions for 2016 day 6

---
+++ 2016/day06/task1.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+
+def main():
+    with open("input", "r") as file:
+        data = [line.strip() for line in file.readlines()]
+
+    data = [[row[i] for row in data] for i in range(len(data[0]))]
+    message = []
+    for line in data:
+        frequnecy = defaultdict(lambda: 0)
+        for letter in line:
+            frequnecy[letter] += 1
+
+        maxLetter, _ = max(frequnecy.items(), key=lambda x: x[1])
+        message.append(maxLetter)
+
+    print("".join(message))
+
+
+if __name__ == "__main__":
+    main()
