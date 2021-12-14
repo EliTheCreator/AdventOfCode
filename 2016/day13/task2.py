ee67b93243228584869668c900b7c75d76e74080
Elias Schmid <elischmi@ethz.ch>
2021-12-14 01:20:42 +0000

    Add solutions for 2016 day 13

---
+++ 2016/day13/task2.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+
+class customdefaultdict(defaultdict):
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError((key,))
+        self[key] = value = self.default_factory(key)
+        return value
+
+
+def isWall(c, fav):
+    x, y = c
+    if x < 0 or y < 0:
+        return 1
+    val = x*x + 3*x + 2*x*y + y + y*y + fav
+    return sum((1 for x in bin(val)[2:] if x == "1")) % 2
+
+
+def main():
+    with open("input", "r") as file:
+        fav = int(file.readline().strip())
+
+    grid = customdefaultdict(lambda c: isWall(c, fav))
+
+    neighbours = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    visited = set()
+    queue = [(1, 1)]
+    nextQueue = []
+    for _ in range(51):
+        while queue:
+            cur = queue.pop(0)
+            if cur not in visited:
+                visited.add(cur)
+                x, y = cur
+                for dx, dy in neighbours:
+                    if not grid[(x + dx, y + dy)]:
+                        nextQueue.append((x + dx, y + dy))
+
+        queue = nextQueue
+        nextQueue = []
+
+    print(len(visited))
+
+
+if __name__ == "__main__":
+    main()
