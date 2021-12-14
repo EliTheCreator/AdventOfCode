a7539e47c1ad893c437900174f8fed34b0859ea5
Elias Schmid <elischmi@ethz.ch>
2021-12-14 20:54:13 +0000

    Add solutions for 2016 day 15

---
+++ 2016/day15/task2.py
@@ -0,0 +1,35 @@
+from functools import reduce
+import re
+
+# Chinese Remainder Theorem
+# https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+# https://www.youtube.com/watch?v=ru7mWZJlRQg
+
+
+def get_Ni(Mi, mi):
+    for Ni in range(mi):
+        if (Mi * Ni) % mi == 1:
+            return Ni
+
+
+def main():
+    with open("input", "r") as file:
+        data = [[int(x) for x in re.findall(r"\d+", line)]
+                for line in file.readlines()]
+
+    chineseRemainder = [(ticks, -1 * ((pos + disc) % ticks))
+                        for disc, ticks, _, pos in data]
+    chineseRemainder.append((11, -1 * ((len(chineseRemainder) + 1) % 11)))
+    M = reduce(lambda x, y: x * y[0], chineseRemainder, 1)
+
+    x = 0
+    for mi, ai in chineseRemainder:
+        Mi = M//mi
+        x += ai * Mi * get_Ni(Mi, mi)
+    x = x % M
+
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
