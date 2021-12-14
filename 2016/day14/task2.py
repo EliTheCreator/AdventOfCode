50650747f92e583bee75688abdee5656be557de7
Elias Schmid <elischmi@ethz.ch>
2021-12-14 18:43:31 +0000

    Add solutions for 2016 day 14

---
+++ 2016/day14/task2.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+from hashlib import md5
+from itertools import count
+from re import search
+
+
+def main():
+    with open("input", "r") as file:
+        salt = file.readline().strip()
+
+    keys = []
+    candidates = defaultdict(lambda: set())
+    for index in count(1):
+        hash = md5(f"{salt}{index}".encode()).hexdigest()
+        for _ in range(2016):
+            hash = md5(f"{hash}".encode()).hexdigest()
+
+        if match5 := search(r"(.)\1{4}", hash):
+            letter = hash[match5.start()]
+            for candidate in sorted(candidates[letter]):
+                if index - candidate <= 1000:
+                    keys.append(candidate)
+                    if len(keys) == 64:
+                        print(sorted(keys)[-1])
+                        return
+            candidates[letter] = set()
+
+        if match3 := search(r"(.)\1{2}", hash):
+            letter = hash[match3.start()]
+
+            candidates[letter].add(index)
+
+
+if __name__ == "__main__":
+    main()
