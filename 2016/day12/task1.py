f5cd142e13270e91a5332e6df49c5d6911f9a25c
Elias Schmid <elischmi@ethz.ch>
2021-12-14 00:42:47 +0000

    Add solutions for 2016 day 12

---
+++ 2016/day12/task1.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+def main():
+    with open("/home/yurigagarin/AdventOfCode/2016/day12/input", "r") as file:
+        instructions = [line.strip().split(" ") for line in file.readlines()]
+
+    regNames = "abcd"
+    regs = defaultdict(lambda: 0)
+    pc = 0
+    while True:
+        instr = instructions[pc]
+        pc += 1
+        match instr:
+            case "cpy", x, y:
+                regs[y] = regs[x] if x in regNames else int(x)
+            case "inc", x:
+                regs[x] += 1
+            case "dec", x:
+                regs[x] -= 1
+            case "jnz", x, y:
+                cond = regs[x] if x in regNames else int(x)
+                val = regs[y] if y in regNames else int(y)
+                if cond:
+                    pc += val - 1
+            case _:
+                print(f"Invalid instruction: {instr}")
+
+        if pc >= len(instructions):
+            break
+
+    print(regs["a"])
+
+
+if __name__ == "__main__":
+    main()
