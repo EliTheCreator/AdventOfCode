fd60cac82f50adc9c1a43c215fe909f3875e2c8b
Elias Schmid <elischmi@ethz.ch>
2021-12-22 22:09:42 +0000

    Add solutions for 2017 day 6

---
+++ 2017/day06/task2.py
@@ -0,0 +1,29 @@
+import re
+
+
+def main():
+    with open("input", "r") as file:
+        banks = [int(x) for x in re.findall(r"\d+", file.readline())]
+
+    cycles = 0
+    configurations = {}
+    while True:
+        config = tuple(banks)
+        if config in configurations.keys():
+            print(cycles - configurations[config])
+            return
+
+        configurations[config] = cycles
+        cycles += 1
+
+        i, val = max(enumerate(banks), key=lambda x: x[1])
+
+        banks[i] = 0
+        j = i
+        for _ in range(val):
+            j = (j + 1) % len(banks)
+            banks[j] += 1
+
+
+if __name__ == "__main__":
+    main()
