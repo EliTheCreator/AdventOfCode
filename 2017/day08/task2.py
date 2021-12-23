3e80acfe2bf89b8d8e38701908eb6f8a45bd5eac
Elias Schmid <elischmi@ethz.ch>
2021-12-23 01:47:27 +0000

    Add solutions for 2017 day 8

---
+++ 2017/day08/task2.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+
+def main():
+    with open("input", "r") as file:
+        instructions = [[part.split(" ") for part in line.strip().split(" if ")]
+                        for line in file.readlines()]
+
+    regs = defaultdict(lambda: 0)
+    maxVal = 0
+    for action, condition in instructions:
+        condRegVal = regs[condition[0]]
+        condConstVal = int(condition[2])
+        condVal = 0
+        match condition[1]:
+            case ">":
+                condVal = condRegVal > condConstVal
+            case ">=":
+                condVal = condRegVal >= condConstVal
+            case "<":
+                condVal = condRegVal < condConstVal
+            case "<=":
+                condVal = condRegVal <= condConstVal
+            case "==":
+                condVal = condRegVal == condConstVal
+            case "!=":
+                condVal = condRegVal != condConstVal
+            case _:
+                pass
+
+        if condVal:
+            actRegName = action[0]
+            actConstVal = int(action[2])
+            match action[1]:
+                case "inc":
+                    regs[actRegName] += actConstVal
+                case "dec":
+                    regs[actRegName] -= actConstVal
+                case _:
+                    pass
+
+            maxVal = max(regs[actRegName], maxVal)
+
+    print(maxVal)
+
+
+if __name__ == "__main__":
+    main()
