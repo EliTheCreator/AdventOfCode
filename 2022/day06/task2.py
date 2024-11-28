993537a11501c1687e23b6789073bd8b899f79ba
Elias Schmid <elias_schmid@hotmail.com>
2024-11-28 23:18:42 +0100

    Add solutions for 2022 day 6

---
+++ 2022/day06/task2.py
@@ -0,0 +1,14 @@
+
+def main():
+    with open("input", "r") as file:
+        stream = file.read().strip()
+
+    marker_length = 14
+    for end in range(marker_length, len(stream)+1):
+        if len(set(stream[end-marker_length:end])) == marker_length:
+            print(end)
+            break
+
+
+if __name__ == "__main__":
+    main()
