403ab301a411d57c1b2fd5546dc513416c1dd59a
Elias Schmid <elias_schmid@hotmail.com>
2024-11-28 23:26:09 +0100

    Add solution for 2022 day 6 part 2

--- 2022/day06/task2.py
+++ 2022/day06/task2.py
@@ -4,10 +4,32 @@ def main():
         stream = file.read().strip()
 
     markerLength = 14
-    for end in range(markerLength, len(stream)+1):
-        if len(set(stream[end-markerLength:end])) == markerLength:
-            print(end)
-            break
+
+    uniqueLetters = 0
+    curLetters = [0 for _ in range(ord('z') + 1)]
+    for l in range(markerLength):
+        letterNum = ord(stream[l])
+        curLetters[letterNum] += 1
+        if curLetters[letterNum] == 1:
+            uniqueLetters += 1
+
+    tail = 0
+    head = markerLength
+    while uniqueLetters < markerLength:
+        tailLetterNum = ord(stream[tail])
+        curLetters[tailLetterNum] -= 1
+        if curLetters[tailLetterNum] == 0:
+            uniqueLetters -= 1
+
+        headLetterNum = ord(stream[head])
+        curLetters[headLetterNum] += 1
+        if curLetters[headLetterNum] == 1:
+            uniqueLetters += 1
+
+        head += 1
+        tail += 1
+
+    print(head)
 
 
 if __name__ == "__main__":
