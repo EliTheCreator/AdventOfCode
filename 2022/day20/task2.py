787fc5134d831c22c1f97735b034ad4f6cbcf742
Elias Schmid <elias_schmid@hotmail.com>
2024-11-28 23:28:23 +0100

    Add solutions for 2022 day 20

---
+++ 2022/day20/task2.py
@@ -0,0 +1,69 @@
+from re import findall
+
+
+class Node():
+    value: int
+    prev: "Node"
+    next: "Node"
+
+    def __init__(self, value):
+        self.value = value
+
+
+def main():
+    with open("input", "r") as file:
+        data = [int(x)*811589153 for x in findall(r"-?\d+", file.read())]
+
+    first: Node = Node(data[0])
+    first.prev = first
+    first.next = first
+    last: Node = first
+    nodeIndex: dict[Node] = {}
+    nodeIndex[0] = first
+    zeroIndex = 0
+    for index, value in enumerate(data[1:], 1):
+        if value == 0:
+            zeroIndex = index
+        curNode = Node(value)
+        nodeIndex[index] = curNode
+        curNode.prev = last
+        curNode.next = first
+        first.prev = curNode
+        last.next = curNode
+        last = curNode
+
+    for _ in range(10):
+        for index in range(len(data)):
+            cur: Node = nodeIndex[index]
+            steps = cur.value%(len(data)-1)
+            if steps == 0:
+                continue
+
+            prev = cur.prev
+            next = cur.next
+
+            prev.next = next
+            next.prev = prev
+
+            for _ in range(steps):
+                next = next.next
+
+            prev = next.prev
+            prev.next = cur
+            cur.prev = prev
+            next.prev = cur
+            cur.next = next
+
+    total = 0
+    cur: Node = nodeIndex[zeroIndex]
+    steps = 1000%len(data)
+    for _ in range(3):
+        for _ in range(steps):
+            cur = cur.next
+        total += cur.value
+
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
